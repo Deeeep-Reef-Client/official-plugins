70c879ad926c411bf68e2f5e6f1334a33d27a9c4
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2023-08-13 21:58:51 +0800

    Background music manager modal should have overlay

--- background-music/_plugin.js
+++ background-music/_plugin.js
@@ -49,7 +49,7 @@ const backgroundmusic_backgroundMusicManagerDiv = DRC.Modal.buildModal("backgrou
                             d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                     </svg>New</button>
 <div id="backgroundmusic_musicList"></div>
-`, true);
+`);
 
 const backgroundmusic_newButton = document.getElementById("backgroundmusic_newButton");
 const backgroundmusic_musicList = document.getElementById("backgroundmusic_musicList");
